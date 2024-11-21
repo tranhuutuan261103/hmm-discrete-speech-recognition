1eb8d6219f3cfb0bc5f46eb75aa6407175ee68e1
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-11-21 22:28:09 +0700

    feat: [HMM] Training model

---
+++ preprocessing.py
@@ -0,0 +1,32 @@
+import math
+
+import librosa
+import numpy as np
+
+
+def get_mfcc(file_path):
+    y, sr = librosa.load(file_path)  # read .wav file
+    hop_length = math.floor(sr * 0.010)  # 10ms hop
+    win_length = math.floor(sr * 0.025)  # 25ms frame
+    # mfcc is 12 x T matrix
+    mfcc = librosa.feature.mfcc(
+        y=y, sr=sr, n_mfcc=12, n_fft=1024,
+        hop_length=hop_length, win_length=win_length)
+    # subtract mean from mfcc --> normalize mfcc
+    mfcc = mfcc - np.mean(mfcc, axis=1).reshape((-1, 1))
+    # delta feature 1st order and 2nd order
+    delta1 = librosa.feature.delta(mfcc, order=1)
+    delta2 = librosa.feature.delta(mfcc, order=2)
+    # X is 36 x T
+    X = np.concatenate([mfcc, delta1, delta2], axis=0)  # O^r
+    # return T x 36 (transpose of X)
+    return X.T  # hmmlearn use T x N matrix
+
+# tim bo tham so, sieu tham so toi uu
+# bao cao ket quâ
+# do chinh xac
+# loss function
+# giai ma o dau, toi uu o dau
+
+# dac trung pho
+# vi sao tim chuoi trang thai toi ưu
